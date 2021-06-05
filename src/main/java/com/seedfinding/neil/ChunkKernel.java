c91a537d530f30e7c9982c61af12378ae430c8ce
Neil <h66r10@gmail.com>
2021-06-05 21:54:35 +0200

    oopsie

--- src/main/java/com/seedfinding/neil/ChunkKernel.java
+++ src/main/java/com/seedfinding/neil/ChunkKernel.java
@@ -15,84 +15,80 @@ import java.util.function.Predicate;
 public class ChunkKernel implements Runnable {
 	public static Predicate<Biome> SANDY = b -> b == Biomes.BEACH;
 	public static Predicate<Biome> OTHER = b -> b != Biomes.BEACH;
-	public static HashMap<Pair<Integer, Integer>, Predicate<Biome>> MASK1 = new HashMap<Pair<Integer, Integer>, Predicate<Biome>>() {
-		{
-			put(new Pair<>(7, 4), OTHER),
-					put(new Pair<>(8, 3), SANDY),
-					put(new Pair<>(8, 4), OTHER),
-					put(new Pair<>(9, 3), SANDY),
-					put(new Pair<>(9, 4), SANDY),
-					put(new Pair<>(9, 5), OTHER),
-					put(new Pair<>(10, 4), SANDY),
-					put(new Pair<>(10, 5), OTHER),
-					put(new Pair<>(11, 4), SANDY),
-					put(new Pair<>(11, 5), OTHER),
-					put(new Pair<>(11, 6), OTHER),
-					put(new Pair<>(12, 5), OTHER),
-					put(new Pair<>(13, 5), OTHER),
-		}
-	};
-	public static HashMap<Pair<Integer, Integer>, Predicate<Biome>> MASK2 = new HashMap<Pair<Integer, Integer>, Predicate<Biome>>() {
-		{
-			put(new Pair<>(8, 5), OTHER),
-					put(new Pair<>(8, 6), OTHER),
-					put(new Pair<>(8, 7), OTHER),
-					put(new Pair<>(8, 8), OTHER),
-					put(new Pair<>(8, 9), OTHER),
-					put(new Pair<>(8, 10), OTHER),
-					put(new Pair<>(8, 11), OTHER),
-					put(new Pair<>(8, 12), OTHER),
-					put(new Pair<>(9, 5), SANDY),
-					put(new Pair<>(9, 6), SANDY),
-					put(new Pair<>(9, 7), OTHER),
-					put(new Pair<>(9, 8), OTHER),
-					put(new Pair<>(9, 9), OTHER),
-					put(new Pair<>(9, 10), SANDY),
-					put(new Pair<>(9, 11), SANDY),
-					put(new Pair<>(9, 12), SANDY),
-					put(new Pair<>(9, 13), SANDY),
-					put(new Pair<>(9, 14), SANDY),
-					put(new Pair<>(9, 15), SANDY),
-					put(new Pair<>(10, 6), SANDY),
-					put(new Pair<>(10, 7), SANDY),
-					put(new Pair<>(10, 8), OTHER),
-					put(new Pair<>(10, 9), SANDY),
-					put(new Pair<>(10, 10), SANDY),
-					put(new Pair<>(10, 11), SANDY),
-					put(new Pair<>(10, 12), SANDY),
-					put(new Pair<>(10, 14), SANDY),
-					put(new Pair<>(10, 15), SANDY),
-					put(new Pair<>(11, 7), SANDY),
-					put(new Pair<>(11, 8), SANDY),
-					put(new Pair<>(11, 9), SANDY),
-					put(new Pair<>(11, 10), SANDY),
-					put(new Pair<>(11, 11), SANDY),
-					put(new Pair<>(11, 14), SANDY),
-					put(new Pair<>(11, 15), SANDY),
-					put(new Pair<>(12, 8), SANDY),
-					put(new Pair<>(12, 9), SANDY),
-					put(new Pair<>(12, 10), SANDY),
-					put(new Pair<>(12, 14), SANDY),
-					put(new Pair<>(12, 15), SANDY),
-					put(new Pair<>(13, 15), SANDY),
-		}
-	};
+	public static HashMap<Pair<Integer, Integer>, Predicate<Biome>> MASK1 = new HashMap<Pair<Integer, Integer>, Predicate<Biome>>() {{
+		put(new Pair<>(7, 4), OTHER);
+		put(new Pair<>(8, 3), SANDY);
+		put(new Pair<>(8, 4), OTHER);
+		put(new Pair<>(9, 3), SANDY);
+		put(new Pair<>(9, 4), SANDY);
+		put(new Pair<>(9, 5), OTHER);
+		put(new Pair<>(10, 4), SANDY);
+		put(new Pair<>(10, 5), OTHER);
+		put(new Pair<>(11, 4), SANDY);
+		put(new Pair<>(11, 5), OTHER);
+		put(new Pair<>(11, 6), OTHER);
+		put(new Pair<>(12, 5), OTHER);
+		put(new Pair<>(13, 5), OTHER);
+	}};
+	public static HashMap<Pair<Integer, Integer>, Predicate<Biome>> MASK2 = new HashMap<Pair<Integer, Integer>, Predicate<Biome>>() {{
+		put(new Pair<>(8, 5), OTHER);
+		put(new Pair<>(8, 6), OTHER);
+		put(new Pair<>(8, 7), OTHER);
+		put(new Pair<>(8, 8), OTHER);
+		put(new Pair<>(8, 9), OTHER);
+		put(new Pair<>(8, 10), OTHER);
+		put(new Pair<>(8, 11), OTHER);
+		put(new Pair<>(8, 12), OTHER);
+		put(new Pair<>(9, 5), SANDY);
+		put(new Pair<>(9, 6), SANDY);
+		put(new Pair<>(9, 7), OTHER);
+		put(new Pair<>(9, 8), OTHER);
+		put(new Pair<>(9, 9), OTHER);
+		put(new Pair<>(9, 10), SANDY);
+		put(new Pair<>(9, 11), SANDY);
+		put(new Pair<>(9, 12), SANDY);
+		put(new Pair<>(9, 13), SANDY);
+		put(new Pair<>(9, 14), SANDY);
+		put(new Pair<>(9, 15), SANDY);
+		put(new Pair<>(10, 6), SANDY);
+		put(new Pair<>(10, 7), SANDY);
+		put(new Pair<>(10, 8), OTHER);
+		put(new Pair<>(10, 9), SANDY);
+		put(new Pair<>(10, 10), SANDY);
+		put(new Pair<>(10, 11), SANDY);
+		put(new Pair<>(10, 12), SANDY);
+		put(new Pair<>(10, 14), SANDY);
+		put(new Pair<>(10, 15), SANDY);
+		put(new Pair<>(11, 7), SANDY);
+		put(new Pair<>(11, 8), SANDY);
+		put(new Pair<>(11, 9), SANDY);
+		put(new Pair<>(11, 10), SANDY);
+		put(new Pair<>(11, 11), SANDY);
+		put(new Pair<>(11, 14), SANDY);
+		put(new Pair<>(11, 15), SANDY);
+		put(new Pair<>(12, 8), SANDY);
+		put(new Pair<>(12, 9), SANDY);
+		put(new Pair<>(12, 10), SANDY);
+		put(new Pair<>(12, 14), SANDY);
+		put(new Pair<>(12, 15), SANDY);
+		put(new Pair<>(13, 15), SANDY);
+	}};
 	public static HashMap<Pair<Integer, Integer>, Predicate<Biome>> MASK3 = new HashMap<Pair<Integer, Integer>, Predicate<Biome>>() {{
-		put(new Pair<>(5, 4), OTHER),
-				put(new Pair<>(5, 5), OTHER),
-				put(new Pair<>(5, 6), OTHER),
-				put(new Pair<>(6, 5), SANDY),
-				put(new Pair<>(7, 5), SANDY),
-				put(new Pair<>(7, 6), SANDY),
-				put(new Pair<>(7, 7), OTHER),
-				put(new Pair<>(8, 6), SANDY),
-				put(new Pair<>(8, 7), OTHER),
-				put(new Pair<>(9, 6), SANDY),
-				put(new Pair<>(9, 7), SANDY),
-				put(new Pair<>(10, 6), SANDY),
-				put(new Pair<>(10, 7), OTHER),
-				put(new Pair<>(11, 6), SANDY),
-				put(new Pair<>(11, 7), OTHER),
+		put(new Pair<>(5, 4), OTHER);
+		put(new Pair<>(5, 5), OTHER);
+		put(new Pair<>(5, 6), OTHER);
+		put(new Pair<>(6, 5), SANDY);
+		put(new Pair<>(7, 5), SANDY);
+		put(new Pair<>(7, 6), SANDY);
+		put(new Pair<>(7, 7), OTHER);
+		put(new Pair<>(8, 6), SANDY);
+		put(new Pair<>(8, 7), OTHER);
+		put(new Pair<>(9, 6), SANDY);
+		put(new Pair<>(9, 7), SANDY);
+		put(new Pair<>(10, 6), SANDY);
+		put(new Pair<>(10, 7), OTHER);
+		put(new Pair<>(11, 6), SANDY);
+		put(new Pair<>(11, 7), OTHER);
 	}};
 
 
@@ -111,9 +107,9 @@ public class ChunkKernel implements Runnable {
 	}
 
 	public static void execute(BiomeSource biomeSource, int chunkX, int chunkZ) {
-		executeSingle(biomeSource,chunkX,chunkZ,MASK1);
-		executeSingle(biomeSource,chunkX,chunkZ,MASK2);
-		executeSingle(biomeSource,chunkX,chunkZ,MASK3);
+		executeSingle(biomeSource, chunkX, chunkZ, MASK1);
+		executeSingle(biomeSource, chunkX, chunkZ, MASK2);
+		executeSingle(biomeSource, chunkX, chunkZ, MASK3);
 	}
 
 	public static void executeSingle(BiomeSource biomeSource, int chunkX, int chunkZ, HashMap<Pair<Integer, Integer>, Predicate<Biome>> hashSet) {
